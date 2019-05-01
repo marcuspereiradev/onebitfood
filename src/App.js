ab34d77eac451296ed3560fbcab075f92cf58b99
marcuspereiradev <vinnycoast@gmail.com>
2019-05-01 10:28:40 -0300

    Creating header and styles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import "./App.scss";
 import { Container, Section } from 'rbx';
-
+import Header from "./components/header";
+ 
 const App = () => (
   <div>
+    <Header />
     <Section>
       <Container>
-        Marcus
+        One Bit Food
       </Container>
     </Section>
   </div>
 )
-
+ 
 export default App;
