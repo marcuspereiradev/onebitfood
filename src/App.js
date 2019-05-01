db61ff9fc15264071a938cc3ed60c29c32c35455
marcuspereiradev <vinnycoast@gmail.com>
2019-05-01 10:59:50 -0300

    Creating home

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import "./App.scss";
 import { Container, Section } from 'rbx';
 import Header from "./components/header";
+import Home from './screens/home';
  
 const App = () => (
   <div>
     <Header />
     <Section>
       <Container>
-        One Bit Food
+        <Home />
       </Container>
     </Section>
   </div>
