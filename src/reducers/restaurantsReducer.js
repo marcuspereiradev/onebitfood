deab1fb11dd110f32ce435199c58b2d4746f7ce7
marcuspereiradev <vinnycoast@gmail.com>
2019-05-04 11:10:21 -0300

    Defining actions types within constants

--- src/reducers/restaurantsReducer.js
+++ src/reducers/restaurantsReducer.js
@@ -1,6 +1,8 @@
+import { LOAD_RESTAURANTS } from '../actions/action_types';
+
 export default (state = { restaurants: [] }, action) => {
   switch(action.type) {
-    case 'LOAD_RESTAURANTS':
+    case LOAD_RESTAURANTS:
       return { 
         ...state,
         restaurants: action.restaurants
