319681af7f3e91bb4866b740aea2c5d45fc9fcb0
marcuspereiradev <vinnycoast@gmail.com>
2019-05-06 12:02:51 -0300

    Fix the code to loadCategories work

--- src/services/api.js
+++ src/services/api.js
@@ -4,8 +4,8 @@ const url = axios.create({ baseURL: 'http://localhost:3001' });
 
 export default {
   loadRestaurants: (category = null) => {
-    let filter = category ? '?category=${category.title}' : ""
+    let filter = category ? `?category=${category.title}` : ""
     return url.get(`/restaurants${filter}`)
   },
-  loadRestaurants: () => url.get('/restaurants')
+  loadCategories: () => url.get('/categories')
 }
