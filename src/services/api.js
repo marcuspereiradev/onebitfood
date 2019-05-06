2fcb06eca1f694ea7980574dfb2a242ba492cb1e
marcuspereiradev <vinnycoast@gmail.com>
2019-05-06 11:23:54 -0300

    Add a method to load the categories

--- src/services/api.js
+++ src/services/api.js
@@ -3,5 +3,9 @@ import axios from 'axios';
 const url = axios.create({ baseURL: 'http://localhost:3001' });
 
 export default {
+  loadRestaurants: (category = null) => {
+    let filter = category ? '?category=${category.title}' : ""
+    return url.get(`/restaurants${filter}`)
+  },
   loadRestaurants: () => url.get('/restaurants')
 }
