bb37819a5baf057f4f54083c50ce5be015d937b6
marcuspereiradev <vinnycoast@gmail.com>
2019-05-09 15:48:37 -0300

    Adding Redux Persist

--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import Store from './store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor } from './store/persisted_store'
 
 ReactDOM.render(
   <Provider store={Store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
